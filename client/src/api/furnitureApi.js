442f240a72a030b94b3bc7f62d72d80769c1dc78
Bobcho101 <bgbobi878@gmail.com>
2025-03-20 11:38:50 +0200

    i mean furniture

--- client/src/api/furnitureApi.js
+++ client/src/api/furnitureApi.js
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react"
 
-export const useGames = () => {
-    const [games, setGames] = useState([]);
+export const useFurniture = () => {
+    const [furniture, setFurniture] = useState([]);
 
     useEffect(() => {
         fetch('http://localhost:3030/data/furniture')
             .then(res => res.json())
-            .then(data => setGames(data.results))
+            .then(data => setFurniture(data.results))
     }, []);
 
-    return [games];
+    return [furniture];
 }
