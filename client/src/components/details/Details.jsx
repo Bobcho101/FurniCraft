52fd60ec61e023ae7b5e79ab49f292bd06a293c0
Bobcho101 <bgbobi878@gmail.com>
2025-03-22 18:53:08 +0200

    added a loading spinner

--- client/src/components/details/Details.jsx
+++ client/src/components/details/Details.jsx
@@ -1,70 +1,61 @@
 import { Link, useParams } from "react-router";
 import { useOneFurniture, useRecommendedFurniture } from "../../api/furnitureApi";
+import { useEffect, useState } from "react";
 
-
-const recommendations = [
-    {
-        id: 1,
-        name: "Luxury Leather Sofa",
-        category: "Living Room",
-        price: "$799",
-        image: "https://via.placeholder.com/300x200",
-    },
-    {
-        id: 2,
-        name: "Minimalist Wooden Chair",
-        category: "Living Room",
-        price: "$149",
-        image: "https://via.placeholder.com/300x200",
-    },
-    {
-        id: 3,
-        name: "Glass Coffee Table",
-        category: "Kitchen",
-        price: "$199",
-        image: "https://via.placeholder.com/300x200",
-    },
-];
 
 export default function Details() {
     const { itemId } = useParams();
     const [ furniture ] = useOneFurniture(itemId);
     const [ recommendedFurniture ] = useRecommendedFurniture(furniture.category, furniture._id);
+    const [ loading, setLoading ] = useState(true);
 
+    
+    useEffect(() => {
+        if (furniture && furniture._id && recommendedFurniture) {
+            setLoading(false);
+        }
+    }, [furniture, recommendedFurniture])
 
+    
     return (
         <>
+        {loading && 
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-75 z-50">
+            <div className="w-16 h-16 border-4 border-gray-300 border-t-indigo-500 rounded-full animate-spin"></div>
+        </div>
+        }
         <div className="bg-gray-900 text-white min-h-screen flex flex-col items-center py-10">
             <div className="bg-gray-800 p-10 rounded-lg shadow-lg max-w-5xl w-full flex flex-col lg:flex-row gap-10 mt-20">
-                <div className="w-full lg:w-1/2">
-                    <img
-                        src={furniture.image}
-                        alt={furniture.name}
-                        className="w-full h-auto rounded-lg"
-                    />
-                </div>
+            <div className="w-full lg:w-1/2">
+                <img
+                    src={furniture.image}
+                    alt={furniture.name}
+                    className="w-full h-auto rounded-lg"
+                />
+            </div>
 
-                <div className="w-full lg:w-1/2">
-                    <h2 className="text-4xl font-bold">{furniture.name}</h2>
-                    <p className="text-indigo-400 text-lg mt-2">{furniture.category}</p>
-                    <p className="text-gray-300 mt-4">{furniture.description}</p>
-                    <p className="text-2xl font-semibold text-white mt-4">${furniture.price}</p>
+            <div className="w-full lg:w-1/2">
+                <h2 className="text-4xl font-bold">{furniture.name}</h2>
+                <p className="text-indigo-400 text-lg mt-2">{furniture.category}</p>
+                <p className="text-gray-300 mt-4">{furniture.description}</p>
+                <p className="text-2xl font-semibold text-white mt-4">${furniture.price}</p>
 
-                    <button className="mt-48 w-full bg-indigo-600 cursor-pointer text-white py-3 px-6 rounded-lg hover:bg-indigo-500 focus:outline-none">
-                        Buy Now
-                    </button>
+                <button className="mt-48 w-full bg-indigo-600 cursor-pointer text-white py-3 px-6 rounded-lg hover:bg-indigo-500 focus:outline-none">
+                    Buy Now
+                </button>
 
-                    <div className="mt-5 text-center">
-                        <Link
-                            to="/catalog/1"
-                            className="text-indigo-400 hover:text-indigo-300 text-sm font-medium"
-                        >
-                            Back to Catalog
-                        </Link>
-                    </div>
+                <div className="mt-5 text-center">
+                    <Link
+                        to="/catalog/1"
+                        className="text-indigo-400 hover:text-indigo-300 text-sm font-medium"
+                    >
+                        Back to Catalog
+                    </Link>
                 </div>
             </div>
-            <div className="mt-12 max-w-5xl w-full">
+        </div>
+            
+        <div className="mt-12 max-w-5xl w-full">
                 <h3 className="text-3xl font-semibold mb-6 text-center">You May Also Like</h3>
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
                     {recommendedFurniture.map((item) => (
