1244f501f3d042fa1ab9a2ed9fe196d6b79b1e88
Bobcho101 <bgbobi878@gmail.com>
2025-03-26 16:40:49 +0200

    added getUserPosts hook

--- client/src/api/userApi.js
+++ client/src/api/userApi.js
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react"
+const baseUrl = 'http://localhost:3030';
 
 export const useGetUserInfo = (accessToken) => {
     const [ userData, setUserData ] = useState({}); 
     useEffect(() => {
-        fetch('http://localhost:3030/users/me', {
+        fetch(baseUrl + '/users/me', {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
@@ -16,4 +17,23 @@ export const useGetUserInfo = (accessToken) => {
     }, [accessToken]);
 
     return [userData];
+}
+
+export const useGetUserPosts = (userId) => {
+    const [ loading, setLoading ] = useState(true);                
+    const [ userPosts, setUserPosts ] = useState([]); 
+
+    useEffect(() => {
+        setLoading(true);
+        fetch(baseUrl + `/data/furniture?where=_ownerId%3D%22${userId}%22`)
+        .then((res) => res.json())
+        .then((data) => {
+            setUserPosts(data);
+            setLoading(false);
+        })
+        .catch((err) => console.log(err))
+    }, [userId]);
+
+
+    return [ userPosts, loading ];
 }
